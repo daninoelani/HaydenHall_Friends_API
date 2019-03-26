320257325da78743c26f6d00d6678cb76617c584
Michael Corey <fishincorey@gmail.com>
2019-03-26 08:24:20 -0600

    cleared out most comments to clean up code

--- index.js
+++ index.js
@@ -2,22 +2,10 @@ const { send, json } = require('micro')
 const cors = require('micro-cors')()
 const { router, get, post, put, del } = require('microrouter')
 
-// const parse = require('urlencoded-body-parser');
-
 let friends = [
     {
         "_id": 1,
-        "name": "Laura",
-        "age": 20
-    },
-    {
-        "_id": 2,
-        "name": "Legend",
-        "age": 20
-    },
-    {
-        "_id": 3,
-        "name": "Mike",
+        "name": "Hayden",
         "age": 20
     }
 ]
@@ -30,47 +18,29 @@ const getFriends = async (req, res) => {
     return send(res, 200, results)
 }
 
+
 // below is working
 const getFriend = async (req, res) => {
-    // const index = await friends.findIndex(friend => friend._id == req.params._id);  
     const results = await friends.filter(friend => friend._id == req.params._id)
-    // console.log("index:", index)
-    // console.log("results", results)
     return send(res, 200, results)
 }
 
 // below is working
 const deleteFriend = async (req, res) => {
-    // friends = await friends.remove({_id: req.params.id});
-    // return send(res, 200, results)
     const index = await friends.findIndex(friend => friend._id == req.params._id);
-    console.log("index:", index)
     const results = await friends.splice(index, 1);
     return send(res, 200, results)
 }
 
 // below is working
-// const createFriend = async (req, res) => {
-//     const friend = await parse(req)
-//         .then(req.query._id = friends.length + 1)
-//         .then(this.friend = req.query)
-//         .catch(err => console.log(err))
-//     const results = await friends.push(this.friend);
-//     console.log("this.friend:", this.friend)
-//     return send(res, 201, results)
-// }
-
-// below is working
 const createFriend = async (req, res) => {
     const data = await json(req)
-    console.log("data:", data)
     const friend = await data
     friend["_id"]= friends.length + 1
     friend["name"]= data.name
     friend["age"]= data.age
     const results = await friends.push(friend);
-    console.log("friend:", friend)
-    return send(res, 200, results), console.log("results:", results)
+    return send(res, 200, results)
 }
 
 // below is working
@@ -78,11 +48,10 @@ const updateFriend = async (req, res) => {
     const data = await json(req)
     const index = await friends.findIndex(friend => friend._id == req.params._id)
     const friend = await friends[index]
-    console.log("index:", index, "data", data)
     friend["name"]= data.name
     friend["age"]= data.age
     const results = await friends.splice(index, 1, friend);
-    return send(res, 200, results), console.log("results:", results)
+    return send(res, 200, results)
 
 }
 
